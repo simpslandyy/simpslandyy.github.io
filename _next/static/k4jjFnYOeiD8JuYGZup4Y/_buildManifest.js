3eb8db8d8ef8285dfef860846eabd362eea412d0
Landy Simpson <simpslandyy@gmail.com>
2020-09-03 02:04:43 -0400

    Add out folder

---
+++ _next/static/k4jjFnYOeiD8JuYGZup4Y/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fcss\u002Fdac44c5ddb394baed3af.css","static\u002Fchunks\u002Fpages\u002Findex-7c5b62f30d174db44b26.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fa39c7ce12d3ae2cec40.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
