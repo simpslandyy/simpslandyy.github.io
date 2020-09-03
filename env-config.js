2d61b1b668f111782ba0a293d2098eadc848a6f9
Landy Simpson <simpslandyy@gmail.com>
2020-09-03 02:34:50 -0400

    change env-config to return function

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === 'production'
 
-module.exports = {
-  'process.env.BACKEND_URL': prod ? '/simpslandyy.github.io' : '',
+module.exports = () => {
+  return { 'process.env.BACKEND_URL': prod ? '/simpslandyy.github.io' : ''}
 }
