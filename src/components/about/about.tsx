4834f82bad29b288f39592fcf0054d442842f7f2
Landy Simpson <simpslandyy@gmail.com>
2020-12-18 02:48:19 -0500

    moved assets to s3 bucket

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Layout from '@components/layout/layout'
 import styles from './about.module.scss'
-import profileImg from '@assets/images/profile.jpg'
 import data from '../../../content/data.md'
 
 const About: React.FunctionComponent = () => {
   const { html, attributes: { about } } = data
+  const profileImg = 'https://blogv2.s3.ca-central-1.amazonaws.com/profile.jpg'
   return (
     <Layout id="about" title="About">
       <div className={styles.about__wrapper}>
