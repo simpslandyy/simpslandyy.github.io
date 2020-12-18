31d774c8a2d2f04a572b64e3d50766a1207aeedd
Landy Simpson <simpslandyy@gmail.com>
2020-12-18 02:49:40 -0500

    Deploy Next.js to gh-pages

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0fc612a9bee293663f08";
+/******/ 	var hotCurrentHash = "5b6b02e0ee04cd545903";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
