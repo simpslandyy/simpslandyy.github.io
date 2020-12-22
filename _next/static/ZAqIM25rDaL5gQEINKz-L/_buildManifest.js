dfee5dd4d189c3058c7beb8fddbb564c3c0829d6
Landy Simpson <simpslandyy@gmail.com>
2020-12-22 00:07:27 -0500

    Deploy Next.js to gh-pages

--- _next/static/ZAqIM25rDaL5gQEINKz-L/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fcss\u002F1d4fc049ce10a98d3d35.css","static\u002Fchunks\u002Fpages\u002Findex-06ef186e0a100e08e55d.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fa39c7ce12d3ae2cec40.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5b6b02e0ee04cd545903";
+/******/ 	var hotCurrentHash = "2ff19d19099f5f8e55f6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
